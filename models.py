8cfb14904cb131d75c0390d6984a7f860950bc2a
Tanya <tanya.chowdhury@myntra.com>
2018-12-14 19:14:41 +0530

    More changes in adding hierarchy

--- models.py
+++ models.py
@@ -176,12 +176,13 @@ class StructureModel():
         mask_sents = self.t_variables['mask_sents'][:, :max_answers,:max_doc_l]
         mask_answers = self.t_variables['mask_answers'][:,:max_answers]
 
-        [_, _, _, rnn_size] = tokens_input.get_shape().as_list()
-        tokens_input_do = tf.reshape(tokens_input, [batch_l * max_doc_l, max_sent_l, rnn_size])
+        [_, _, _, _, rnn_size] = tokens_input.get_shape().as_list()
+        tokens_input_do = tf.reshape(tokens_input, [batch_l * max_answers,max_doc_l, max_sent_l, rnn_size])
 
         sent_l = tf.reshape(sent_l, [batch_l * max_doc_l])
         mask_tokens = tf.reshape(mask_tokens, [batch_l * max_doc_l, -1])
 
+        #Word level input
         tokens_output, _ = dynamicBiRNN(tokens_input_do, sent_l, n_hidden=self.config.dim_hidden,
                                         cell_type=self.config.rnn_cell, cell_name='Model/sent')
         
@@ -212,6 +213,7 @@ class StructureModel():
             tokens_output = tokens_output + tf.expand_dims((mask_tokens-1)*999,2)
             tokens_output = tf.reduce_max(tokens_output, 1)
 
+        #Sentence level RNN
         sents_input = tf.reshape(tokens_output, [batch_l, max_doc_l, 2*self.config.dim_sem])
         sents_output, _ = dynamicBiRNN(sents_input, doc_l, n_hidden=self.config.dim_hidden, cell_type=self.config.rnn_cell, cell_name='Model/doc')
 
@@ -233,6 +235,30 @@ class StructureModel():
         elif (self.config.doc_attention == 'max'):
             sents_output = sents_output + tf.expand_dims((mask_sents-1)*999,2)
             sents_output = tf.reduce_max(sents_output, 1)
+
+        #Answer level RNN
+        sents_input = tf.reshape(tokens_output, [batch_l, max_doc_l, 2*self.config.dim_sem])
+        sents_output, _ = dynamicBiRNN(sents_input, doc_l, n_hidden=self.config.dim_hidden, cell_type=self.config.rnn_cell, cell_name='Model/doc')
+
+        sents_sem = tf.concat([sents_output[0][:,:,:self.config.dim_sem], sents_output[1][:,:,:self.config.dim_sem]], 2)
+        sents_str = tf.concat([sents_output[0][:,:,self.config.dim_sem:], sents_output[1][:,:,self.config.dim_sem:]], 2)
+
+        str_scores_ = get_structure('doc', sents_str,max_doc_l, self.t_variables['mask_parser_1'], self.t_variables['mask_parser_2'])  #batch_l,  sent_l+1, sent_l
+        str_scores = tf.matrix_transpose(str_scores_)  # soft parent
+        sents_sem_root = tf.concat([tf.tile(embeddings_root, [batch_l, 1, 1]), sents_sem], 1)
+        sents_output_ = tf.matmul(str_scores, sents_sem_root)
+        sents_output = LReLu(tf.tensordot(tf.concat([sents_sem, sents_output_], 2), w_comb, [[2], [0]]) + b_comb)
+
+        if (self.config.doc_attention == 'sum'):
+            sents_output = sents_output * tf.expand_dims(mask_sents,2)
+            sents_output = tf.reduce_sum(sents_output, 1)
+        elif (self.config.doc_attention == 'mean'):
+            sents_output = sents_output * tf.expand_dims(mask_sents,2)
+            sents_output = tf.reduce_sum(sents_output, 1)/tf.expand_dims(tf.cast(doc_l,tf.float32),1)
+        elif (self.config.doc_attention == 'max'):
+            sents_output = sents_output + tf.expand_dims((mask_sents-1)*999,2)
+            sents_output = tf.reduce_max(sents_output, 1)
+
 
         #Placeholders for decoder inputs and targets
         targets = tf.placeholder(tf.int32, [None, None], name='targets') 
