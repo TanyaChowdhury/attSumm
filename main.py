c5c5ea6a074854cce3a3104effdb6d6342a678ba
tanya14109 <tanya14109@iiitd.ac.in>
2019-01-20 02:33:15 +0530

    gpu param added

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from models import StructureModel
 import tqdm
 
 def load_data(config):
-    train, dev, test, embeddings, vocab = cPickle.load(open(config.data_file))
+    try:
+        train, dev, test, embeddings, vocab = cPickle.load(open(config.data_file))
+    except:
+        print 'pickle unloading error'
     trainset, devset, testset = DataSet(train), DataSet(dev), DataSet(test)
     vocab = dict([(v.index,k) for k,v in vocab.items()])
     trainset.sort()
